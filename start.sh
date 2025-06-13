7fdb17f38ce9866b061bcf2da8053e22024215f0
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2025-06-13 21:35:54 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Start QEMU VM with VNC output on :1 (5901)
+qemu-system-x86_64 \
+  -m 4096 \
+  -drive file=win10.qcow2,format=qcow2 \
+  -boot c \
+  -net nic -net user \
+  -vga std \
+  -usb -device usb-tablet \
+  -rtc base=localtime \
+  -smp 2 \
+  -vnc :1 \
+  -daemonize
+
+# Start noVNC to expose VNC via WebSocket on port 6080
+/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080
