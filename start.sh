58fdf534855b3303ef062c21ce6c74d20f2e3ccb
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2025-06-15 08:00:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ set -e
 # Start QEMU VM with VNC output on :1 (5901)
 qemu-system-x86_64 \
   -m 4096 \
-  -drive file=win7.qcow2,format=qcow2 \
-  -boot d \
+  -drive file=win2019.qcow2,format=qcow2 \
+  -boot c \
   -net nic -net user \
   -vga std \
   -usb -device usb-tablet \
